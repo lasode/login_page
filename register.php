28ae503b168ab8e16a750da1547f7903945c4a24
lasode <sammielas88@gmail.com>
2019-09-17 15:58:28 +0100

    first Commit

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'config.php';
+
+if (isset($_POST["u_btn"])){
+  $u_name = $_POST["u_name"];
+  $u_email = $_POST["u_email"];
+  $u_pass = $_POST["u_pass"];
+
+if (empty($u_name) || empty($u_email)|| empty($u_pass)) {
+  echo "please complete all Data";
+} else{
+  $insert = mysqli_query($conn,"INSERT INTO `users` (`u_name`, `u_email`, `u_pass`) VALUES ( '$u_name', 'u_email', 'u_pass')");
+  echo "Success";
+}
+
+}
+
+?>
+
+
+
+
+
+<form action="register.php" method="post">
+<label>Name</label>
+<input type="text" name="u_name" value=""><br>
+<label>Email</label>
+<input type="text" name="u_email" value=""><br>
+<label>Password</label>
+<input type="password" name="u_pass" value=""><br>
+<input type="submit" name="u_btn" value="sign up">
+
+</form> 
